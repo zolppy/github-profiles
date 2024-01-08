358f1a31f0a81f5d811425bac20db029159d2971
zolppy <gabriel.lcifba@gmail.com>
2024-01-08 17:47:33 -0300

    leve estilização, dados vazios de repositórios não são mostrados e mais uma informação

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,6 @@ async function getProfile(profileName) {
 
   const response = await fetch(`${API_URL}/${profileName}`);
 
-  console.log(response.ok);
-  
   if (response.ok) {
     const data = await response.json();
     
@@ -27,12 +25,22 @@ async function getRepos(profileName) {
   return data;
 }
 
+// Formata data no estilo "aaaa-mm-dd" para "dd/mm/aaaa"
+function formatDate(date) {
+  var part = date.split("-");
+  
+  var formatedDate = part[2] + "/" + part[1] + "/" + part[0];
+  
+  return formatedDate;
+}
+
 function showProfileInfo(profile) {
   const profileImageContainer = document.querySelector("#profile-img");
   const profileNameContainer = document.querySelector("#profile-name");
   const profileBioContainer = document.querySelector("#profile-bio");
   const profileFollowersContainer = document.querySelector("#profile-followers-number");
   const profileFollowingContainer = document.querySelector("#profile-following-number");
+  const profileCreationDateContainer = document.querySelector("#creation-date-date");
   const profileReposNumberContainer = document.querySelector("#profile-repos-number");
   const profileCompanyNameContainer = document.querySelector("#profile-company-name");
 
@@ -41,6 +49,7 @@ function showProfileInfo(profile) {
   let profileBio = profile.bio;
   let profileFollowers = profile.followers;
   let profileFollowing = profile.following;
+  let creationDate = formatDate(profile.created_at.slice(0, 10));
   let profileReposNumber = profile.public_repos;
   let profileCompanyName = profile.company;
 
@@ -49,31 +58,56 @@ function showProfileInfo(profile) {
   profileBioContainer.textContent = profileBio;
   profileFollowersContainer.textContent = profileFollowers;
   profileFollowingContainer.textContent = profileFollowing;
+  profileCreationDateContainer.textContent = creationDate;
   profileReposNumberContainer.textContent = profileReposNumber;
   profileCompanyNameContainer.textContent = profileCompanyName;
 }
 
 function showRepoList(repos) {
+  let repoId = 0;
   const repoListContainer = document.querySelector("#repo-list");
-
   const currentRepos = repoListContainer.querySelectorAll(".repo");
 
   for (const repo of currentRepos) {
     repo.remove();
   }
 
+  
   for (const repo of repos) {
+    repoId++;
+    let repoName = repo.name;
+    let repoDescription = repo.description;
+    let repoLanguage = repo.language;
+    let repoUrl = repo.html_url;
+    let repoTopics = repo.topics;
+
+    if (repo.topics.length > 0) {
+      repoTopics = String(repoTopics);
+  
+      if (repoTopics.includes(",")) {
+        repoTopics = repoTopics.replaceAll(",", ", ");
+      }
+    } else {
+      repoTopics = "";
+    }
+
     const repoElement = `
-      <div class="repo">
-        <p class="repo-name">Nome: ${repo.name}</p>
-        <p class="repo-description">Descrição: ${repo.description}</p>
-        <p class="repo-language">Linguagem: ${repo.language}</p>
-        <p class="repo-topics">Tópicos: ${[...repo.topics]}</p>
-        <p class="repo-link">Link: <a href="${repo.html_url}">Abrir</a></p>
+      <div class="repo" id="repo-${repoId}">
+        <p class="repo-name">Nome: <span class="repo-detail">${repoName}</span></p>
+        <p class="repo-description">Descrição: <span class="repo-detail">${repoDescription}</span></p>
+        <p class="repo-language">Linguagem: <span class="repo-detail">${repoLanguage}</span></p>
+        <p class="repo-topics">Tópicos: <span class="repo-detail">${repoTopics}</span></p>
+        <p class="repo-link">Link: <span class="repo-detail"><a href="${repoUrl}">Abrir</a></span></p>
       </div>
     `;
 
     repoListContainer.innerHTML += repoElement;
+
+    const thisRepo = document.querySelector(`#repo-${repoId}`);
+
+    !repoDescription && thisRepo.querySelector(".repo-description").remove();
+    !repoLanguage && thisRepo.querySelector(".repo-language").remove();
+    !repoTopics && thisRepo.querySelector(".repo-topics").remove();
   }
 }
 
